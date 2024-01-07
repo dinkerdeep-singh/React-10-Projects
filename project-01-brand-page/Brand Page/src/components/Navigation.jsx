f4c5e972fe43d4fcf958f8f97762b29ef753f2e3
dinkersin <dinkerdeep@gmail.com>
2024-01-07 07:47:57 +0530

    1st React Project: Brand Page

---
+++ project-01-brand-page/Brand Page/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return (
+        <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>login</button>
+      </nav>
+    )
+}
+
+export default Navigation
