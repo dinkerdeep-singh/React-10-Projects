0f33ff59ced5a1c619ac8c57abd29d6e433369fa
dinkersin <dinkerdeep@gmail.com>
2024-01-08 20:38:53 +0530

    2nd React Project: Contact Us Page

---
+++ project-02-contact-us-page/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Button from './components/Button/Button'
+import ContactForm from './components/ContactForm/ContactForm'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
+
+  return (
+    <div>
+      <Navigation />
+      <main className='main_container'>
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </div>
+  )
+}
+
+export default App
