4c2cf18f08e44161cdc8bd1008a9991d676d80fe
Dinkerdeep Singh <dinkerdeep@gmail.com>
2024-02-04 08:55:30 +0530

    Project 05: Modal onClose bug fix

--- project-05-firebase-app/src/components/Modal.jsx
+++ project-05-firebase-app/src/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ onClose, isOpen, children }) => {
     <>
         {isOpen && (
         <>
-        <div onClick={onClose} className="grid place-items-center backdrop-blur h-screen w-screen absolute top-0 z-40">
+        <div className="grid place-items-center backdrop-blur h-screen w-screen absolute top-0 z-40">
           <div className="m-auto z-50 relative min-h-[200px] min-w-[80%] bg-white p-4">
                 <div className="flex justify-end">
                     <AiOutlineClose onClick={onClose} className="text-2xl"/>
